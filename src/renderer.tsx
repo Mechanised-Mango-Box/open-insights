34791140780868616f7206c3ae1fc9c084aed380
Mechanised Mango Box <mechanised_mango_box@proton.me>
2026-03-23 12:55:30 +1100

    Added react

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -28,6 +28,17 @@
 
 import './index.css';
 
-console.log(
-  '👋 This message is being logged by "renderer.ts", included via Vite',
+// console.log(
+//   '👋 This message is being logged by "renderer.ts", included via Vite',
+// );
+
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './ui/MainMenu';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
