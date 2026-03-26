75c522a06e7b0e39afbbd94bf70e8fac970ec910
chap989 <rcha0087@student.monash.edu>
2026-03-26 21:59:51 +1100

    video pauses after a certian time

--- youtube_quiz/content.js
+++ youtube_quiz/content.js
@@ -1,2 +1,15 @@
-console.log("extension loaded")
+console.log("EXTENSION LOADED")
 
+let quiz_shown = false
+function time_check() {
+    const video = document.querySelector('video')
+    console.log(video)
+    console.log(video.currentTime)
+    
+    if (video.currentTime >= 10 && quiz_shown === false) {
+        quiz_shown = true
+        video.pause()
+    }
+}
+
+const intervalID = setInterval(time_check, 1000) ;
