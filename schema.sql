432d93ebc9dc84538f911d9c4bf95a3351872afa
chap989 <rcha0087@student.monash.edu>
2026-07-13 13:52:07 +1000

    Started working on create a DB for video records to go in.

---
+++ schema.sql
@@ -0,0 +1,12 @@
+-- Creating a TABLE for videos 
+-- the variables/coloumns we need are video_id, youtube_url, duration_min, word_count, wpm, scene_count, scene_rate, uploader_opt_in
+
+CREATE TABLE videos (
+    video_id        INT PRIMARY KEY,
+    youtube_url     VARCHAR(50),
+    duration_min    INTEGER,
+    word_count      INTEGER,
+    wpm             REAL,
+    scene_count     INTEGER,
+    uploader_opt_in INTEGER -- i guess this is binary yes or no   
+);
