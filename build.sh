5805c83823a33bdce08a60f0fe963e6928c3af18
Mechanised Mango Box <mechanised_mango_box@proton.me>
2026-08-05 21:47:37 +1000

    Github actions to build for all platforms

--- build.sh
+++ build.sh
@@ -1,1 +1,18 @@
-pyinstaller --onefile --add-data "assets:assets" --collect-data imgui_bundle main.py
+#!/bin/bash
+set -e
+
+echo "Starting PyInstaller build..."
+
+# unix = ':', win = ';'
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
+    SEPARATOR=";"
+    echo "Targeting Windows..."
+else
+    SEPARATOR=":"
+    echo "Targeting Unix..."
+fi
+
+# Run PyInstaller
+pyinstaller --onefile --add-data "assets${SEPARATOR}assets" --collect-data imgui_bundle main.py
+
+echo "Build complete."
