8476f1aa667e72eb15d8fa11417cd38b7449d04f
Mubashar Doostizadah <mdoo0013@student.monash.edu>
2026-08-06 16:04:09 +1000

    Create model training directory/file structure

--- model_training/data_preparation.py
+++ model_training/data_preparation.py
@@ -1,6 +1,8 @@
 """
 Prepare data for model training. Right now we have TrainingResourcesSnapshot, however we don't currently have the extracted 
 features such as wpm. This file will create the data in a format that can be used to train a model by combining the TrainingResourcesSnapshot
-and the extracted features. 
+and the extracted features.  
+It is important that the features are normalised using z-score standardisation to achieve a mean of 0 and a standard 
+deviation of 1.
 Not sure if we need this file though, as this may be done elsewhere.
 """
